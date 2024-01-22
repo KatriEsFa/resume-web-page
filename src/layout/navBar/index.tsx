c50dd906392fb6941e5a15efe8d5e4760cc7a12a
KatriEsFa <katriesfa@gmail.com>
2024-01-22 19:55:33 +1000

    deleted logo from navbar, styles finished for desktop

--- src/layout/navBar/index.tsx
+++ src/layout/navBar/index.tsx
@@ -1,19 +1,17 @@
 import { NavWraper } from "./assets/NavWraper"
 import NavButton from "./components/NavButton"
-import LogoImage from "../../assets/no-background.jpg"
-import { LogoWraper } from "./assets/LogoWraper"
 import styled from "styled-components"
 
 const StyledNavBar = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: center;
 `
 
 const NavBar = () => {
 
   return (
     <StyledNavBar>
-        <LogoWraper link={LogoImage} alt="Web Page Logo" />
         <NavWraper>
             <NavButton text="Home" link="/ruta-a-personaStyledLizar"/>
             <NavButton text="About" link="/ruta-a-personaStyledLizar"/>
